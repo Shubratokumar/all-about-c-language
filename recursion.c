7d93cb6290ecdee3a74d8a4f65a576f3070c967d
Shubrato Kumar <shubratokumargharami920@gmail.com>
2025-05-28 16:19:16 +0600

    recursion with example

--- recursion.c
+++ recursion.c
@@ -1,21 +1,47 @@
 #include <stdio.h>
 // recursion
-void printH(int count);
+
+// Practice Q No:30 : Sum of first n natural numbers;
+int sum(int n);
 int main(){
-    int a;
-    printf("Enter count: ");
-    scanf("%d", &a);
-    printH(a);
-    return 0;
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    printf("Sum of first n : %d \n", sum(n));
+}
+// recursive function
+int sum(int n){
+    //base case 
+    if (n == 1)
+    {
+        return 1;
+    }    
+    // sum of 1 to n
+    int sumofNminus1 = sum(n-1);
+    // printf("Sum of n minus 1: %d \n", sumofNminus1);
+    int sumofN = sumofNminus1 + n;
+    // printf("Sum of n : %d \n", sumofN);
+    return sumofN;
 }
 
+
+
+// void printH(int count);
+// int main(){
+//     int a;
+//     printf("Enter count: ");
+//     scanf("%d", &a);
+//     printH(a);
+//     return 0;
+// }
+
 // recursive function
-void printH(int count){
-    if (count == 0)
-    {
-        return;
-    }
-    printf("Hello, Recursion! \n");
-    printH(count -1); // calling itself
+// void printH(int count){
+//     if (count == 0)
+//     {
+//         return;
+//     }
+//     printf("Hello, Recursion! \n");
+//     printH(count -1); // calling itself
     
-}
+// }
