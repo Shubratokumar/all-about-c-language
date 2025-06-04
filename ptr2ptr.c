2c92c69367d9a71f1f775b55dd9809f46f00e617
Shubrato Kumar <shubratokumargharami920@gmail.com>
2025-06-04 11:23:48 +0600

    ptr 2 ptr examples

--- ptr2ptr.c
+++ ptr2ptr.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 int main(void){
-    float price = 100.00;
-    float *ptr = &price;
-    float **pptr = &ptr;
-    printf("%f \n", *ptr);
-    printf("%f", **pptr);
+    // float price = 100.00;
+    // float *ptr = &price;
+    // float **pptr = &ptr;
+    // printf("%f \n", *ptr);
+    // printf("%f", **pptr);
+    // print the value of 'i' from its pointer to pointer
+    int i = 5;
+    int *ptr = &i;
+    int **pptr = &ptr;
+    printf("%d \n", *ptr);
+    printf("%d \n", *(&i));
+    printf("%d \n", *(&ptr));
+    printf("%d \n", *pptr);
+    printf("%d \n", *(&pptr));
+    printf("%d \n", **pptr);
 
     return 0;
 }
