caf71af303abac687274d7ea2c0d8ed8e332bedc
Shubrato Kumar <shubratokumargharami920@gmail.com>
2025-06-10 19:07:22 +0600

    number compare using if else

--- if-else.c
+++ if-else.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 
-int main(void) {
-    int age;
-    printf("Enter age : ");
-    scanf("%d", &age);
-    if (age >= 18)
+int main(void)
+{
+    // number comparision
+    int num1, num2;
+
+    printf("Enter two numbers to compare :\n");
+
+    scanf("%d %d", &num1, &num2);
+
+    if (num1 > num2)
     {
-        printf("Adult. can vote as well as drive. \n");        
+        printf("%d is greater than %d", num1, num2);
     }
-    else if(age > 12 & age < 18)
+    else
     {
-        printf("Teenager! \n");
-    }
-    else{
-        printf("Child! \n");
+        printf("%d is greater than %d", num2, num1);
     }
 
+    // age comparision
+    // int age;
+    // printf("Enter age : ");
+    // scanf("%d", &age);
+    // if (age >= 18)
+    // {
+    //     printf("Adult. can vote as well as drive. \n");
+    // }
+    // else if(age > 12 & age < 18)
+    // {
+    //     printf("Teenager! \n");
+    // }
+    // else{
+    //     printf("Child! \n");
+    // }
+    return 0;
 }
