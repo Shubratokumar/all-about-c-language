a91adb24d4bdd26ff4cea9cef140b6f22a7be983
Shubrato Kumar <shubratokumargharami920@gmail.com>
2025-06-24 13:49:59 +0600

    string: take string input from the user using %c - while loop

--- string.c
+++ string.c
@@ -2,19 +2,39 @@
 #include <string.h>
 
 int main(){
+    // take a string input from the user using %c(while loop)
+    char str[100];
+    char ch;
+    int i = 0;
+    printf("Enter String: ");
+    // condition: until press enter
+    while (ch != '\n')
+    {
+
+        scanf("%c", &ch);
+        str[i] = ch;
+        i++;
+    }
+    //puting null character
+    str[i]= '\0';
+    puts(str);
+
     // strcmp: string comparision
-    char first[]= "Chocolate";
+
+    // char first[]= "Chocolate";
     // char first[]= "Banana";// will return 0;
-    char second[]= "Banana";
-    printf("%d", strcmp(first, second));
+    // char second[]= "Banana";
+    // printf("%d", strcmp(first, second));
 
     // strcat: string concatenation
+
     // char first[]= "Shubrato ";
     // char second[]= "Kumar";
     // strcat(first, second);
     // puts(first);
 
     // strcpy: copying string
+
     // char oldStr[] = "Old String";
     // char newStr[] = "New String";
     // strcpy(newStr, oldStr);
@@ -22,6 +42,7 @@ int main(){
     // printf("New string %s", newStr);
 
     // strlen: printing string length using library
+
     // char name[]= "Shubrato";
     // printf("Length is : %d", strlen(name));
 
