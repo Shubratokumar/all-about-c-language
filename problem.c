cb20296fb8efa456e53d73ab6e72539cbbb932a9
Shubrato Kumar <shubratokumargharami920@gmail.com>
2025-06-27 13:49:02 +0600

    hrproblem: using for loop print odd and even number

--- problem.c
+++ problem.c
@@ -1,18 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
-// Complete the function void update(int *a,int *b). It receives two integer pointers, int* a and int* b. Set the value of  to their sum, and  to their absolute difference. There is no return value, and no return statement is needed.
-void update(int *a,int *b) {
-    int sum = *a + *b;
-    int diff = abs(*a - *b); // absolute difference
-    *a = sum;
-    *b = diff;   
+int main()
+{
+    // Given a five digit integer, print the sum of its digits.
 }
-int main() {
-    int a, b;
-    int *pa = &a, *pb = &b;    
-    scanf("%d %d", &a, &b);
-    update(pa, pb);
-    printf("%d\n%d", a, b);
+// using for loop print odd and even number
+// int a, b;
+// scanf("%d\n%d", &a, &b);
+// for (int i = a; i <= b; i++)
+// {
+//     if (i >= 1 && i <= 9)
+//     {
+//         switch (i)
+//         {
+//         case 1:
+//             printf("one\n");
+//             break;
+//         case 2:
+//             printf("two\n");
+//             break;
+//         case 3:
+//             printf("three\n");
+//             break;
+//         case 4:
+//             printf("four\n");
+//             break;
+//         case 5:
+//             printf("five\n");
+//             break;
+//         case 6:
+//             printf("six\n");
+//             break;
+//         case 7:
+//             printf("seven\n");
+//             break;
+//         case 8:
+//             printf("eight\n");
+//             break;
+//         case 9:
+//             printf("nine\n");
+//             break;
+//         }
+//     }
+//     else
+//     {
+//         if (i % 2 == 0)
+//         {
+//             printf("even \n");
+//         }
+//         else
+//         {
+//             printf("odd \n");
+//         }
+//     }
+// }
+// return 0;
 
-    return 0;
-}
+// Complete the function void update(int *a,int *b). It receives two integer pointers, int* a and int* b. Set the value of  to their sum, and  to their absolute difference. There is no return value, and no return statement is needed.
+// void update(int *a,int *b) {
+//     int sum = *a + *b;
+//     int diff = abs(*a - *b); // absolute difference
+//     *a = sum;
+//     *b = diff;
+// }
+// int main() {
+//     int a, b;
+//     int *pa = &a, *pb = &b;
+//     scanf("%d %d", &a, &b);
+//     update(pa, pb);
+//     printf("%d\n%d", a, b);
+
+//     return 0;
+// }
