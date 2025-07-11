e1082537366ec5bec10ffc2a34aa66e8c0c42761
Shubrato Kumar <shubratokumargharami920@gmail.com>
2025-07-11 13:29:15 +0600

    file: program to write all the odd numbers from 1 to n in a file

--- filex.c
+++ filex.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    // Make a program to input student information from a user & enter it to a file.
+    // write a program to write all the odd numbers from 1 to n in a file.
     FILE *shuvo;
-    shuvo = fopen("student.txt", "w");
-    char name[100];
-    int age;
-    float cgpa;
-    printf("Enter name: ");
-    scanf("%s", &name);
-    printf("Enter age: ");
-    scanf("%d", &age);
-    printf("Enter cg: ");
-    scanf("%f", &cgpa);
-
-    fprintf(shuvo, "%s\t", name);
-    fprintf(shuvo, "%d\t", age);
-    fprintf(shuvo, "%.2f\t", cgpa);
-    fclose(shuvo);
+    shuvo = fopen("odd.txt", "w");
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for (int i = 0; i <= n; i++)
+    {
+        if (i % 2 != 0)
+        {
+            fprintf(shuvo,"%d\n", i);
+        }
+        
+    }   
     
+    fclose(shuvo);    
     return 0;
 }
